8808fd10705ee1b13195c1b20a55460aac9f0c23
TetianaL <4215654@gmail.com>
2024-04-14 20:20:33 +0300

    signIn+signUp

--- src/pages/SigninPage/SigninPage.jsx
+++ src/pages/SigninPage/SigninPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import AuthForm from 'components/AuthForm/AuthForm';
+import css from '../../components/AuthForm/AuthForm.module.css';
 
 const SigninPage = () => {
   return (
-    <div>
-      <AuthForm />
-    </div>
+    <section className={`${css.AuthFormSection} container`}>
+      <div>
+        <AuthForm />
+      </div>
+    </section>
   );
 };
 
