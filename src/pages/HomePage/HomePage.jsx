32941dcc64ad394431a52596e3532a6c5bd08ece
Maksym Lytovchenko <lmb1988@ukr.net>
2024-04-14 16:42:39 +0300

    addHomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import css from './HomePage.module.css'
 
+// import DailyNorma  from '';
+// import WaterRatioPanel  from '';
+// import CalendarContainer from '';
+// import TodayWaterList from '';
+
+// import { useAuth } from 'hooks/useAuth';
+// import { useSelector } from 'react-redux';
 const HomePage = () => {
-  return <div>HomePage</div>;
+
+  return (
+    <section className={css.section_HomePage}>
+      <div className={`container ${css.container_HomePage}`}>
+        <div className={css.container_daliNorma}>
+          {/* <DailyNorma />
+          <WaterRatioPanel /> */}
+        </div>
+        
+        <div className={css.container_MonthStats}>
+          {/* <TodayWaterList/>
+          <CalendarContainer /> */}
+        </div>
+      </div>
+    </section>
+  );
 };
+
 
 export default HomePage;
