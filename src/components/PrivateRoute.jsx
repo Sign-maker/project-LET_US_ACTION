a0aebcfcffd8536616ef8d010beb6d105e6290e3
Serhii <xx.code4u@gmail.com>
2024-04-16 22:55:29 +0300

    Update PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,8 +2,7 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from 'hooks/useAuth';
 
 export const PrivateRoute = ({ redirectTo = '/', component: Component }) => {
-  const { isLoggedIn, isRefreshing } = useAuth(true);
-
+  const { isLoggedIn, isRefreshing } = useAuth();
   const shoudRedirect = !isLoggedIn && !isRefreshing;
 
   return shoudRedirect ? <Navigate to={redirectTo} /> : Component;
