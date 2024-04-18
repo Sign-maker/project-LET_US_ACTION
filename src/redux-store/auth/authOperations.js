31142d16f2b9ba5e343532dac6f59743d5616d8d
TetianaL <4215654@gmail.com>
2024-04-18 18:43:55 +0300

    notifications+muDailyNormalModal+operations updateMyDailyNorma

--- src/redux-store/auth/authOperations.js
+++ src/redux-store/auth/authOperations.js
@@ -1,5 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios1 from 'axios';
+import {
+  toastFulfilled,
+  toastRejected,
+} from 'components/servises/UserNotification';
 
 const baseURL = process.env.REACT_APP_BASE_BACKEND_URL;
 
@@ -19,10 +23,23 @@ export const register = createAsyncThunk(
   async (credentials, thunkAPI) => {
     try {
       const { data } = await axios.post('/users/signup', credentials);
+      toastFulfilled('Registration successful!');
       setAuthHeader(data.token);
       return data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
+      switch (error.response) {
+        case 409:
+          toastRejected(
+            `This email is already in use by another user. Please try a different email!`
+          );
+          return thunkAPI.rejectWithValue(error.massage);
+        case 400:
+          toastRejected(`The password must contain at least 8 characters`);
+          return thunkAPI.rejectWithValue(error.massage);
+
+        default:
+          return thunkAPI.rejectWithValue(error.message);
+      }
     }
   }
 );
@@ -32,10 +49,12 @@ export const logIn = createAsyncThunk(
   async (credentials, thunkAPI) => {
     try {
       const { data } = await axios.post('/users/signin', credentials);
+      toastFulfilled('You have successfully logged into your account!');
       setAuthHeader(data.token);
 
       return data;
     } catch (error) {
+      toastRejected('Something went wrong, please try again later!');
       return thunkAPI.rejectWithValue(error.message);
     }
   }
@@ -47,6 +66,7 @@ export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
     clearAuthHeader();
     return data;
   } catch (error) {
+    toastRejected('Something went wrong, please try again later!');
     return thunkAPI.rejectWithValue(error.message);
   }
 });
@@ -56,8 +76,10 @@ export const updateProfile = createAsyncThunk(
   async (credentials, thunkAPI) => {
     try {
       const { data } = await axios.patch('/users/profile', credentials);
+      toastFulfilled('Your data has been successfully updated!');
       return data;
     } catch (error) {
+      toastRejected('Something went wrong, please try again later!');
       return thunkAPI.rejectWithValue(error.message);
     }
   }
@@ -74,9 +96,10 @@ export const updateAvatar = createAsyncThunk(
           'Content-Type': 'multipart/form-data',
         },
       });
-
+      toastFulfilled('Your avatar has been successfully updated!');
       return data.avatarURL;
     } catch (error) {
+      toastRejected('Something went wrong, please try again later!');
       return thunkAPI.rejectWithValue(error.message);
     }
   }
@@ -87,8 +110,10 @@ export const updateAvatar = createAsyncThunk(
 //   async (name, thunkAPI) => {
 //     try {
 //       const { data } = await axios.patch('users/update/name', { name });
+//       toastFulfilled('Your name has been successfully updated!');
 //       return data.name;
 //     } catch (error) {
+//       toastRejected('Something went wrong, please try again later!');
 //       return thunkAPI.rejectWithValue(error.message);
 //     }
 //   }
@@ -107,6 +132,27 @@ export const refreshUser = createAsyncThunk(
       const { data } = await axios.get('/users/current');
       return data;
     } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const updateMyDailyNorma = createAsyncThunk(
+  'user/dailyNorma',
+  async (formData, thunkAPI) => {
+    try {
+      const setMyDailyNorma = async formData => {
+        const { data } = await axios.patch('user/dailyNorma', formData);
+        return data;
+      };
+
+      const data = await setMyDailyNorma(formData);
+      toastFulfilled(
+        'Your daily water allowance has been successfully updated!'
+      );
+      return data.dailyNorma;
+    } catch (error) {
+      toastRejected('Something went wrong, please try again later!');
       return thunkAPI.rejectWithValue(error.message);
     }
   }
